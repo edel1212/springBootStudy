232cc13effedb1cfb33e6b497a79f2f60e87c84e
Yoo <edel1212@naver.com>
2022-09-11 16:34:08 +0900

    add comment

--- ex01/src/main/java/org/zerock/ex01/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/ex01/controller/SampleController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     /**
-     * @Description :
+     * @Description : - Spring Boot는  많은 설정이 없이 JSON Type의 데이터를 반환할때도
+     *                  jackson-databind 라이브러리를 추가해 주지 않아도
+     *                  Spring initializr에서 추가한 Spring Web의 의존성 항목을 추가하여
+     *                  자동으로 추가되어 사용이 가능하다.
+     *
+     *                - @RestController를 이용하여 별도의 화면없이
+     *                  해당 URL 이동 시 JSON type 데이터 전송 확인이 가능하다!
      * **/
     @GetMapping("/hello")
     public String[] hello(){
--- ex01/src/test/java/org/zerock/ex01/Ex01ApplicationTests.java
+++ ex01/src/test/java/org/zerock/ex01/Ex01ApplicationTests.java
@@ -8,6 +8,9 @@ class Ex01ApplicationTests {
 	/**
 	 * @Description : 첫 테스트 코드!
 	 *               🎈 method 실행 단축키 Ctrl + Shift + F10
+	 *
+	 *               Spring Boot의 경우 Spring과 다르게 이미 테스트 환겨잉 갖춰져 있기
+	 *               때문에 별도의 설정없이 테스트 실행이 가능하다
 	 */
 	@Test
 	void contextLoads() {
