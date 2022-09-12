e833d935a6ab308f2c4d565fd5c78f1f86d534dc
Yoo <edel1212@naver.com>
2022-09-12 21:37:29 +0900

    fix README

--- ex01/src/main/java/org/zerock/ex01/Ex01Application.java
+++ ex01/src/main/java/org/zerock/ex01/Ex01Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ex01Application {
 	/**
-	 * @Description : gradle 서버를 기동하는 곳 단축키 Shift + F1
+	 * @Description : gradle 서버를 기동하는 곳 단축키 Shift + F10
 	 *                🎈 내가 run 할 class 를 정해서 하는 방법 Alt + Shift + F10
 	 *
 	 *                🎈 gradle 빌드 단축키 Ctrl + F9
