88cdd7b324d56b3bb3e177b0a897ca26007029d3
Yoo <edel1212@naver.com>
2022-10-23 19:45:05 +0900

    apply Read, Modify , Delete to UI

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -50,5 +51,46 @@ public class BoardController {
         redirectAttributes.addFlashAttribute("msg",bno);
         return "redirect:/board/list";
     }
+    
+    //단건 조회 + 수정 페이지
+    @GetMapping({"/read","/modify"})
+    public void read(@ModelAttribute("requestDTO")PageRequestDTO pageRequestDTO, Long bno, Model model){
+        log.info("bno :::" + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(bno);
+
+        model.addAttribute("dto",boardDTO);
+
+    }
+
+    //수정
+    @PostMapping("/modify")
+    public String modify(BoardDTO boardDTO
+            , @ModelAttribute("requestDTO")PageRequestDTO requestDTO
+            , RedirectAttributes redirectAttributes){
+        log.info("modify Data ::" + boardDTO);
+
+        boardService.modify(boardDTO);
+        
+        // 클라이언트 단에서 받은 데이터를 addFlash 로 list로 다시 넘겨줌
+        redirectAttributes.addFlashAttribute("page", requestDTO.getPage());
+        redirectAttributes.addFlashAttribute("type", requestDTO.getType());
+        redirectAttributes.addFlashAttribute("keyword", requestDTO.getKeyword()); 
+
+        return "redirect:/board/list";
+    }
+
+    //삭제 + 댓글 -- 댓글 먼저 삭제 후 board 삭제
+    @PostMapping("/remove")
+    public String remove(Long bno,RedirectAttributes redirectAttributes){
+        log.info("remove... Bno ::" + bno);
+        boardService.removeWithReplies(bno);
+        redirectAttributes.addFlashAttribute("msg",bno);
+        return "redirect:/board/list";
+    }
+
+
 
 }
