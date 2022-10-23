84dd36e9423c7ff098c117826d8b2190d0a7ae3c
Yoo <edel1212@naver.com>
2022-10-23 14:37:45 +0900

    apply getList to UI

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -2,14 +2,25 @@ package org.zerock.board.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.board.dto.PageRequestDTO;
 import org.zerock.board.service.BoardService;
 
 @Log4j2
 @RequestMapping("/board/")
 @RequiredArgsConstructor
+@Controller
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list ::::" + pageRequestDTO);
+        model.addAttribute("result",boardService.getList(pageRequestDTO));
+    }
 
 }
