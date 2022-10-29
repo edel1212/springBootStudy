f5cee1a670f34951f02188b21d2a1d32cab56d9a
Yoo <edel1212@naver.com>
2022-10-29 17:50:38 +0900

    add ReplyServiceTests File

---
+++ board/src/test/java/org/zerock/board/service/ReplyServiceTests.java
@@ -0,0 +1,13 @@
+package org.zerock.board.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class ReplyServiceTests {
+    @Autowired
+    private ReplyService replyService;
+
+}
