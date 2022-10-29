170bb59a7fbdeb699c9e919f86236a2f8183b93e
Yoo <edel1212@naver.com>
2022-10-29 17:48:51 +0900

    add mdify, remove

--- board/src/main/java/org/zerock/board/service/ReplyServiceImpl.java
+++ board/src/main/java/org/zerock/board/service/ReplyServiceImpl.java
@@ -40,11 +40,12 @@ public class ReplyServiceImpl implements ReplyService{
 
     @Override
     public void modify(ReplyDTO replyDTO) {
-
+        Reply reply = dtoToEntity(replyDTO);
+        replyRepository.save(reply);
     }
 
     @Override
     public void remove(Long rno) {
-
+        replyRepository.deleteById(rno);
     }
 }
