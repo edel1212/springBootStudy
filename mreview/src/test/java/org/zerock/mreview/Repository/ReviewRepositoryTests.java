9a815fb50357731eedca95a5fcf075872336525a
Yoo <edel1212@naver.com>
2022-11-05 21:38:38 +0900

    add getReview info

--- mreview/src/test/java/org/zerock/mreview/Repository/ReviewRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/Repository/ReviewRepositoryTests.java
@@ -8,6 +8,7 @@ import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -50,6 +51,41 @@ public class ReviewRepositoryTests {
 
         });
         
+    }
+
+    @Test
+    public void testGetMovieReviews(){
+        Movie movie = Movie.builder().mno(120L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(data ->{
+            log.info(data.getReviewnum());
+            log.info("--------------------");
+            log.info(data.getGrade());
+            log.info("--------------------");
+            log.info(data.getText());
+            log.info("--------------------");
+            /**
+             * 일반적인 methodName Query 를 사용해서 해당 member 의 email 값을 가져오려하면 Error 발생
+             * -[Error Message] no Session
+             * 이유❔
+             * - Review Class 의 Member 에 대한 Fetch 방식이 Lazy 이기 때문에
+             *   한 번에 Review 객체와 Member 객체를 조회할 수 없기 떄문임
+             *   
+             * ✔ 해결방법
+             *   - 1) @Transactional 을 사용한다 :: 이것은 해당 getMember().getEmail()을 처리할떄마다 조회를
+             *                                    다시 한다는 문제가 있음
+             *   - 2) @Query() JPQL 을 사용하여 Join 을 사용한다
+             *   
+             *   - 3) @EntityGraph 을 이용해서 ㅂReview 객체를 가져올때마다  Member 객체를 로딩 시킨다
+             *   //TODO 이어서 작성 필요
+             *
+             * **/
+            log.info(data.getMember().getEmail());
+            log.info("--------------------");
+        });
+
     }
 
 }
