77fc16a665af12ee127e8cb5d03d68bd43caeb73
Yoo <edel1212@naver.com>
2022-11-12 21:06:46 +0900

    add make thumbnail logic

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -73,6 +74,16 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
             try {
                 uploadFile.transferTo(savePath); //실제 데이터를 저장하는 Logic
+                
+                //썸네일 생성 Thumbnailator 사용
+                //1) 파일명 생성 -- ✔ s_ 를 사용하여 구분함
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator
+                        + "s_" + uuid + "_" + fileName;
+                //2) File 객체 생성
+                File thumbnailFile = new File(thumbnailSaveName);
+                // Thumbnailator 를 사용하여 썸네일 생성 (File inFile, File outFile, width, height)
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100,100);
+
                 resultDTOList.add(UploadResultDTO.builder().fileName(fileName).folderPath(folderPath).uuid(uuid).build());
             }catch (Exception e){
                 e.printStackTrace();
