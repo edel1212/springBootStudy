aa917861062b40b025898d6f3f44dc2adc1ca47a
Yoo <edel1212@naver.com>
2022-11-20 20:14:46 +0900

    add avg, reviewCnt, reg..modDate to MoiveDTO

--- mreview/src/main/java/org/zerock/mreview/dto/MovieDTO.java
+++ mreview/src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +23,17 @@ public class MovieDTO {
 
     private String title;
 
-    private List<MovieImageDTO> imageDTOList;
+    @Builder.Default  // 해당 어노테이션은 객체 생성 시 원하는 값으로 초기화해서 반환 받을 수 있다.
+    private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+    
+    //영화의 평균 평점
+    private double avg;
+
+    //리뷰 수 jpa 의 count();
+    private int reviewCnt;
+    
+    //생성 , 수정일
+    private LocalDateTime regDate, modDate;
+
 
 }
