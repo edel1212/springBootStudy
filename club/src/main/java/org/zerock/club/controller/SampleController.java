97e90d2d6179c66dc7c2085756576fe4af8e3b48
Yoo <edel1212@naver.com>
2022-12-03 22:15:22 +0900

    add write prinipal to console

--- club/src/main/java/org/zerock/club/controller/SampleController.java
+++ club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,9 +1,11 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.club.security.dto.ClubAuthMemberDTO;
 
 @Controller
 @Log4j2
@@ -14,10 +16,16 @@ public class SampleController {
     public void exAll(){
         log.info("exAll.....");
     }
-
+    
+    //@AuthenticationPrincipal 를 사용하면 별도의  케스팅 없이 데이터를
+    //사용이 가능함
     @GetMapping("/member")
-    public void exMember(){
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO){
+        
         log.info("exMember....");
+        
+        log.info("로그인 정보 :::" + clubAuthMemberDTO);
+        
     }
 
     @GetMapping("/admin")
