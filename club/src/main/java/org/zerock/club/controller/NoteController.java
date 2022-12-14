9bb99e746b8e465857494aa890b8b9b239bdd4c9
Yoo <edel1212@naver.com>
2022-12-14 22:34:25 +0900

    add Register Test

--- club/src/main/java/org/zerock/club/controller/NoteController.java
+++ club/src/main/java/org/zerock/club/controller/NoteController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class NoteController {
 
     private final NoteService noteService;
 
+    @PostMapping(value = "")
     public ResponseEntity<Long> register(@RequestBody NoteDTO noteDTO){
 
         log.info("---------------------------------");
@@ -28,6 +30,12 @@ public class NoteController {
         HttpStatus status = num == 0 ? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.OK;
 
         return new ResponseEntity<>(num, status);
+    }
+
+    @PostMapping("stringTest")
+    public String stringResponseTest(@RequestBody NoteDTO noteDTO){
+        log.info(noteDTO);
+        return "Hello!";
     }
 
     //__Eof__
