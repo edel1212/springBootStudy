4631c77c2fe3b6a6f123712533b317681c095a43
Yoo <edel1212@naver.com>
2022-12-17 21:22:14 +0900

    start login api filter

---
+++ club/src/main/java/org/zerock/club/security/filter/ApiLoginFilter.java
@@ -0,0 +1,24 @@
+package org.zerock.club.security.filter;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+public class ApiLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    public ApiLoginFilter(String defaultFilterProcessesUrl) {
+        super(defaultFilterProcessesUrl);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+}
