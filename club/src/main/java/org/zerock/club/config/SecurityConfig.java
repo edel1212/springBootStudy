90e39fde7c1d8fb6bf74213add8bfe276be52816
Yoo <edel1212@naver.com>
2022-12-18 19:33:07 +0900

    add Apit Header security info Check Logic

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.zerock.club.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.zerock.club.security.filter.ApiCheckFilter;
--- club/src/main/java/org/zerock/club/security/filter/ApiCheckFilter.java
+++ club/src/main/java/org/zerock/club/security/filter/ApiCheckFilter.java
@@ -1,7 +1,9 @@
 package org.zerock.club.security.filter;
 
 import lombok.extern.log4j.Log4j2;
+import net.minidev.json.JSONObject;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @Description : 해당 Class 는 필터역활을 하는 Class 이다. 
@@ -43,13 +46,60 @@ public class ApiCheckFilter extends OncePerRequestFilter {
         boolean matchFlag = this.antPathMatcher.match(this.pattern, request.getRequestURI()); //URI 와 URI Pattern 비교
         log.info("matchFlag ::: " + matchFlag);
 
-        if(matchFlag){
+        if(matchFlag){ // 내가 지정한 URI 와 접근한 URI 가 같을 경우 TRUE
             log.info("ApiCheckFilter...........................");
             log.info("ApiCheckFilter...........................");
             log.info("ApiCheckFilter...........................");
-            return;
+            
+            //API 의 Header 값 체크 [내가 지정한값 - 보안을 위함 허용된 사용자만 사용하게끔 하기 위함]
+            boolean checkHeader = this.checkAuthHeader(request);
+            if(checkHeader){
+               filterChain.doFilter(request,response); // 다음 단계로 넘어가는 역할을 해줌
+               return;
+            } else { //인증 Header 값이 다를경우
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN); //403 ERROR
+                //Return JSON encoding Header Setting
+                response.setContentType("application/json;charset=utf-8");
+                JSONObject json = new JSONObject();
+                String message = "FAIL CHECK API TOKEN";
+                json.put("code", 403);
+                json.put("message", message);
+                
+                // HttpServletResponse 를 이용해서
+                // PrintWriter 객체 생성 후 거기에
+                // json 을 담아 전달함.
+                PrintWriter out = response.getWriter();
+                out.println(json);
+                return;
+            }
         }//if
 
         filterChain.doFilter(request,response); // 다음 단계로 넘어가는 역할을 해줌
     }
+
+    /**
+     * @Description : Authorization 해더 처리를 위한 method
+     *                - 특정한 API 를 호출 시 다른  Server 또는 Client 에서 실행 되기 때문에
+     *                  쿠키, 세션을 사용할수 없는 문제가있음
+     *                  ✅ 따라서 해당 정보는 Http Header Message 에 특별한 값을 담아서 전송한다.
+     *
+     *                - Client 에서 전송한 Request 에 포함된 Authorization 해더 값을 파악해서
+     *                  사용자가 정상적인 요청인지 확인함.
+     *
+     * */
+    private boolean checkAuthHeader(HttpServletRequest request){
+        boolean checkResult = false;
+
+        String  authHeader = request.getHeader("Authorization");
+        
+        //내가 정한 값으로 Equals 시키자
+        if("12345678".equals(authHeader)){
+            log.info("Authorization exist : " +authHeader);
+            checkResult = true;
+        }//if
+        
+        return checkResult;
+    }
+
+    //__Eof__
 }
