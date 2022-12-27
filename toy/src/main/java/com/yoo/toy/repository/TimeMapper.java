3cffb5c376f2a7dce57d2234c5317f1d2aee456f
Yoo <edel1212@naver.com>
2022-12-27 23:00:17 +0900

    setup toy project

---
+++ toy/src/main/java/com/yoo/toy/repository/TimeMapper.java
@@ -0,0 +1,8 @@
+package com.yoo.toy.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+public interface TimeMapper {
+
+}
---
+++ toy/src/test/java/com/yoo/toy/repository/TimeTests.java
@@ -0,0 +1,17 @@
+package com.yoo.toy.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class TimeTests {
+
+    @Test
+    public void getTimeTest(){
+        log.info("aaaa");
+    }
+
+}
