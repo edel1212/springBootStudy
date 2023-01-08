b9f076c7bcb682abe5d1ab040c51d6d30ec495ac
Yoo <edel1212@naver.com>
2023-01-08 13:21:53 +0900

    Fix comment

--- board/src/main/java/org/zerock/board/entity/Reply.java
+++ board/src/main/java/org/zerock/board/entity/Reply.java
@@ -20,8 +20,8 @@ public class Reply extends BaseEntity{
 
     private String replyer;
     
-    @ManyToOne(fetch = FetchType.LAZY) // N : 1
-    @ToString.Exclude //ToString 에서 제외시킴 - 이유는 해당 변수를 호출하기 위해서는 데이타ㅓ베이스에 연결이 필요하기 때문임
+    @ManyToOne
+    @ToString.Exclude    //ToString 에서 제외시킴 - 이유는 해당 변수를 호출하기 위해서는 데이타베이스에 연결이 필요하기 때문임
     private Board board; //연관관계 지정 -- > 자동으로 커럼명은 board_bno 로 됨 변수명_PK 로 된다
 
 }
