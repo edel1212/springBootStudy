016cd315fbb8e6af70d13153d4e3f039629e4913
yoo <edel1212@naver.com>
2023-01-26 22:39:35 +0900

    consumes m produces Test Code

--- ex04/src/main/java/com/yoo/ex04/cotroller/ReplyController.java
+++ ex04/src/main/java/com/yoo/ex04/cotroller/ReplyController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,9 @@ public class ReplyController {
         return ResponseEntity.ok().body(replyService.getList(bno));
     }
 
+    @PostMapping(value = "/consumesSuccess", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> consumesTest(){
+        return ResponseEntity.ok().body("success");
+    }
 
 }
