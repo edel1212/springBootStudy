c0e986c8b4b1c00c945383d9cb3f24d1edd282ab
yoo <edel1212@naver.com>
2023-01-29 22:21:37 +0900

    add RestTemplate Test Url

--- board/src/main/java/org/zerock/board/controller/ReplyController.java
+++ board/src/main/java/org/zerock/board/controller/ReplyController.java
@@ -53,4 +53,35 @@ public class ReplyController {
 
     }
 
+    //////////////////////////
+    // ex04에서 요청할 RestTemplate 테스트용 URL
+
+    /**
+     * 알게된 새로운 사실 파리미터인 name을 받을 때
+     * @ReqeustParam을 사용할시 해당 name 파라미터가 강제됨 값이 없을 경우 에러를 반환함.
+     * -> 기본 설정이 필수값으로 설정 되어 있기  때문임.
+     *
+     * ex) (@RequestParam String name)
+     *     -> http://localhost:8080/replies/restTest?name=yoo  :: 이상없음
+     *     -> http://localhost:8080/replies/restTest           :: 에러 발생☠️
+     *      Error Code : 400 [bad Request]
+     *      Error Msg  : Required request parameter 'name' for method
+     *                   parameter type String is not present
+     * 해결방안 : 1) required = boolean  옵션을 사용하여 필수 요소를 지정이 가능하다.
+     *             - (@RequestParam(required = false) String name )
+     *         2) defaultValue = "??"  지정이 가능하다.
+     *             - (@RequestParam(required = false
+     *                            , defaultValue = "흑곰" ) String name )
+     * */
+    @GetMapping("/restTest")
+    public String getStringWithRestTempTest(@RequestParam(required = false) String name ){
+
+        log.info("name ::: {} ", name);
+
+        String result =
+                name != null ? "Hello " + name + " World" : "Hello World";
+
+        return result;
+    }
+
 }
