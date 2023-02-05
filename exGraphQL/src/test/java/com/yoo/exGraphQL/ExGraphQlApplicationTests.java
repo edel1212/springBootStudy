d474f2c4c3981cc0800b2638fef186b753e28213
yoo <edel1212@naver.com>
2023-02-05 22:21:31 +0900

    GraphQL Study Start

---
+++ exGraphQL/src/test/java/com/yoo/exGraphQL/ExGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.exGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
