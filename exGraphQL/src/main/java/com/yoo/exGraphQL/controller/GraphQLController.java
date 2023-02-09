a83c3d976e7617ecafbf5acad0f3594a6229d8bd
yoo <edel1212@naver.com>
2023-02-09 22:45:45 +0900

    CRUD Logic

--- exGraphQL/src/main/java/com/yoo/exGraphQL/controller/GraphQLController.java
+++ exGraphQL/src/main/java/com/yoo/exGraphQL/controller/GraphQLController.java
@@ -1,9 +1,11 @@
 package com.yoo.exGraphQL.controller;
 
+import com.yoo.exGraphQL.dto.MemberDTO;
 import com.yoo.exGraphQL.entity.Member;
 import com.yoo.exGraphQL.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -15,10 +17,40 @@ import java.util.List;
 public class GraphQLController {
     private final MemberService memberService;
 
+    //Query Type
+
     @QueryMapping
-    public List<Member> allFindMembers(){
+    public MemberDTO findMember(String email){
         log.info("----- graphQL QueryType -----");
-        return memberService.getList();
+        return memberService.findMember(email);
+    }
+
+    @QueryMapping
+    public List<MemberDTO> allFindMembers(){
+        log.info("----- graphQL QueryType -----");
+        return memberService.allFindMembers();
+    }
+
+    /////////////////////////////////////////
+
+    //Mutation Type
+
+    @MutationMapping
+    public String registerMember(MemberDTO memberDTO){
+        log.info("----- graphQL MutationType -----");
+        return memberService.registerMember(memberDTO);
+    }
+
+    @MutationMapping
+    public Boolean updateMember(MemberDTO memberDTO){
+        log.info("----- graphQL MutationType -----");
+        return memberService.updateMember(memberDTO);
+    }
+
+    @MutationMapping
+    public Boolean deleteMember(String email){
+        log.info("----- graphQL MutationType -----");
+        return memberService.deleteMember(email);
     }
 
 }
--- exGraphQL/src/main/java/com/yoo/exGraphQL/service/MemberService.java
+++ exGraphQL/src/main/java/com/yoo/exGraphQL/service/MemberService.java
@@ -1,9 +1,47 @@
 package com.yoo.exGraphQL.service;
 
+import com.yoo.exGraphQL.dto.MemberDTO;
 import com.yoo.exGraphQL.entity.Member;
 
+import javax.persistence.Entity;
 import java.util.List;
 
 public interface MemberService {
-    List<Member> getList();
+
+    //Query Type
+    MemberDTO findMember(String email);
+
+    List<MemberDTO> allFindMembers();
+
+    ///////////////////////////////////////////
+
+
+    //Mutation Type
+    String registerMember(MemberDTO member);
+
+    Boolean updateMember(MemberDTO member);
+
+    Boolean deleteMember(String email);
+
+
+    //dtoToEntity
+    default Member dtoToEntity(MemberDTO memberDTO){
+        return Member.builder()
+                .email(memberDTO.getEmail())
+                .password(memberDTO.getPassword())
+                .name(memberDTO.getName())
+                .build();
+    }
+
+    //entityToDto
+    default MemberDTO entityToDto(Member member){
+        return MemberDTO.builder()
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .name(member.getName())
+                .regDate(member.getRegDate())
+                .modDate(member.getModDate())
+                .build();
+    }
+
 }
