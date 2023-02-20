175c0991e52cdd20efbe5f67d6b2440b1e64be08
yoo <edel1212@naver.com>
2023-02-20 22:22:08 +0900

    직렬화 사용 이유

--- mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
+++ mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -14,7 +14,22 @@ import java.nio.charset.StandardCharsets;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+/**
+ * 💬 Serializable(직렬화) 란 ?
+ * - 자바 시스템 내부에서 사용되는 객체 또는 데이터를 외부의 자바 시스템에서도
+ *   사용할 수 있도록 <strong>Byte(바이트)</strong> 형태로 데이터를 변환하는 기술이다.
+ * - 시스템적으로 JVM의 Runtime Data Area(Heap 또는 Stack영역)에 상주하고 있는 객체 데이터를
+ *   Byte(바이트) 형태로 변환하는 기술과 직렬화된 바이트 형태의 데이터를 객체로 변환하여 JVM으로 상주시키는
+ *   형태를 말하기도한다.
+ *
+ * 💬 Serializable(직렬화) 사용이유 ?
+ * - 서블릿 세션들은 대부분의 세션의 Java 직렬화를 지원하고 있다, 단순히 서블릿 메모리 위에서
+ *   어플리케이션을 사용할 것이면 굳이 직렬화가 필요없지만, <strong>파일로 저장하거나
+ *   세션 클러스트링, DB를 저장하는 옵션등을 선택하게되면 직렬화가 필요하다.</strong>
+ * */
 public class UploadResultDTO implements Serializable {
+
+    private static final long serialVersionUID = 5129628467395047900L;
 
     private String fileName;
     private String uuid;
