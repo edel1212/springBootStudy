d838639b8a3f9ec3f68437ff10e0911c2117196b
yoo <edel1212@naver.com>
2023-02-23 22:32:02 +0900

    Add Spring Security Test Code

--- toy/src/main/java/com/yoo/toy/config/SecurityConfig.java
+++ toy/src/main/java/com/yoo/toy/config/SecurityConfig.java
@@ -1,2 +1,19 @@
-package com.yoo.toy.config;public class SecurityConfig {
+package com.yoo.toy.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration //BeanContainer에서 해당 Class를 스캔 가능하게 끔함
+@Log4j2
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity.build();
+    }
+
 }
--- toy/src/main/java/com/yoo/toy/controller/SampleController.java
+++ toy/src/main/java/com/yoo/toy/controller/SampleController.java
@@ -1,2 +1,28 @@
-package com.yoo.toy.controller;public class SampleController {
+package com.yoo.toy.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/sample")
+public class SampleController {
+
+    @GetMapping("/all")
+    public void exAll(){
+        log.info("누구나 접근 가능");
+    }
+
+    @GetMapping("/member")
+    public void exMember(){
+        log.info("Member 이상의 권한만 접근 가능");
+    }
+
+    @GetMapping("/admin")
+    public void exAdmin(){
+        log.info("Admin 이상의 권한만 접근 가능");
+    }
+
 }
