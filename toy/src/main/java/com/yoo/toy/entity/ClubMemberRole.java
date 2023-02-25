fe4a38caf33cab3d17deeef4264e1b96d472cf49
yoo <edel1212@naver.com>
2023-02-25 21:19:04 +0900

    add Security With Database

---
+++ toy/src/main/java/com/yoo/toy/entity/ClubMemberRole.java
@@ -0,0 +1,8 @@
+package com.yoo.toy.entity;
+
+/**
+ * 권한을 지정할 enum Class
+ * */
+public enum ClubMemberRole {
+    USER, MANAGER, ADMIN
+}
---
+++ toy/src/test/java/com/yoo/toy/securityRepository/ClubMemberTests.java
@@ -0,0 +1,10 @@
+package com.yoo.toy.securityRepository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClubMemberTests {
+
+
+
+}
