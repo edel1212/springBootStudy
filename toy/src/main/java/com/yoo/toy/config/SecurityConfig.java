ca5e91791dedffed95d88b8d707c105bbf3ec6f6
yoo <edel1212@naver.com>
2023-02-28 22:53:45 +0900

    add @EnableGlobalMethodSecurity Code

--- toy/src/main/java/com/yoo/toy/config/SecurityConfig.java
+++ toy/src/main/java/com/yoo/toy/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //BeanContainer에서 해당 Class를 스캔하도록 지정
 @Log4j2
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
     /**
@@ -55,58 +57,44 @@ public class SecurityConfig {
         httpSecurity.authenticationManager(authenticationManager);
 
 
+        /***
+         *  formLogin() 이란 ?
+         *  - 인가, 인증에 문제가 생길시 로그인 페이지로 이동하끔 함
+         *
+         * ❌ formLogin()의 단점은 해당 매서드를 이용하는 경우에는 별도의 디자인을 적용 불가능한 Spring Security에서
+         *    제공하는 UI를 사용해야함
+         *
+         * 👉 로그인 화면 , 성공 실패 등 커스텀 페이지 생성 후 URL을 지정하고 싶다면 ?
+         *    loginPage()
+         *    , loginProcessUrl()
+         *    , defaultSuccessUrl()
+         *    , failureUrl()
+         *     등을 이용하면 필요한 설정을 지정할 수있다.
+         *    - 대부분의 어플리게이션은 고유의 디자인을 적용하기 떄문에 loginPage()를 이용해 별도의 페이지를 만들어 사용!
+         **/
+        httpSecurity.formLogin();
+
+        // CSRF를 사용하지 않도록 설정
+        httpSecurity.csrf().disable();
+
         /**
-         * httpSecurity.authorizeRequests()를 사용하여 인증이 필요한 자원들을 설정할 수 있다.
-         * */
-        httpSecurity.authorizeRequests()
-                /**
-                 * antMatchers("targetUrl"); <<- 엔트 패턴으로도 지정이 가능하다
-                 * */
-                // 누구나 로그인 없이도 /sample/all 에 접근 가능
-                .antMatchers("/sample/all").permitAll()
-                //User 권한을 갖으면 /sample/member 에 접근 가능
-                .antMatchers("/sample/member").hasRole("USER")
-
-                .and()      //추가적 옵션을 달수있다.
-
-                /***
-                 *  formLogin() 이란 ?
-                 *  - 인가, 인증에 문제가 생길시 로그인 페이지로 이동하끔 함
-                 *
-                 * ❌ formLogin()의 단점은 해당 매서드를 이용하는 경우에는 별도의 디자인을 적용 불가능한 Spring Security에서
-                 *    제공하는 UI를 사용해야함
-                 *
-                 * 👉 로그인 화면 , 성공 실패 등 커스텀 페이지 생성 후 URL을 지정하고 싶다면 ?
-                 *    loginPage()
-                 *    , loginProcessUrl()
-                 *    , defaultSuccessUrl()
-                 *    , failureUrl()
-                 *     등을 이용하면 필요한 설정을 지정할 수있다.
-                 *    - 대부분의 어플리게이션은 고유의 디자인을 적용하기 떄문에 loginPage()를 이용해 별도의 페이지를 만들어 사용!
-                 **/
-                .formLogin();
-
-                // CSRF를 사용하지 않도록 설정
-                httpSecurity.csrf().disable();
-
-                /**
-                *   Logout 설정
-                * -  로그아웃 페이지 생성
-                * ❌ logout()의 단점은 fromLogin 과 같이 별도 디자인 적용 가능
-                 *
-                *  👉 커스텀 페이지를 사용하려면  ?
-                 *  logoutUrl()
-                 *  , logoutSuccessUrl()
-                 *   등으로  커스텀 페이지 제작 가능
-                 *
-                *   👉 로그아웃 시 쿠키 및 세션 삭제
-                 *   invalidateHttpSession(Boolean)
-                 *   , deleteCookies()
-                 *   를 사용하여 로그웃시 쿠키및 세션을 무효하 설정도 가능하다
-                 *   ex) httpSecurity.logout().deleteCookies().invalidateHttpSession(true);
-                 *
-                * */
-                httpSecurity.logout();
+        *   Logout 설정
+        * -  로그아웃 페이지 생성
+        * ❌ logout()의 단점은 fromLogin 과 같이 별도 디자인 적용 가능
+         *
+        *  👉 커스텀 페이지를 사용하려면  ?
+         *  logoutUrl()
+         *  , logoutSuccessUrl()
+         *   등으로  커스텀 페이지 제작 가능
+         *
+        *   👉 로그아웃 시 쿠키 및 세션 삭제
+         *   invalidateHttpSession(Boolean)
+         *   , deleteCookies()
+         *   를 사용하여 로그웃시 쿠키및 세션을 무효하 설정도 가능하다
+         *   ex) httpSecurity.logout().deleteCookies().invalidateHttpSession(true);
+         *
+        * */
+        httpSecurity.logout();
 
         return httpSecurity.build();
     }
--- toy/src/main/java/com/yoo/toy/controller/SampleController.java
+++ toy/src/main/java/com/yoo/toy/controller/SampleController.java
@@ -1,6 +1,9 @@
 package com.yoo.toy.controller;
 
+import com.yoo.toy.dto.security.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SampleController {
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(){
         log.info("누구나 접근 가능");
     }
 
+    @PreAuthorize("hasRole('MANAGER')")
     @GetMapping("/member")
-    public void exMember(){
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO dto){
+
+        log.info("login info ::: {}",dto);
+
         log.info("Member 이상의 권한만 접근 가능");
     }
 
