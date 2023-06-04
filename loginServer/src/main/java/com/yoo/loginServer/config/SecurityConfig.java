36dd207dbc0b3716ca22f02268a38874a41c91c8
yoo <edel1212@naver.com>
2023-06-04 22:24:25 +0900

    URI Filter 등록

--- loginServer/src/main/java/com/yoo/loginServer/config/SecurityConfig.java
+++ loginServer/src/main/java/com/yoo/loginServer/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.yoo.loginServer.config;
 
+import com.yoo.loginServer.security.filter.ApiCheckFilter;
 import com.yoo.loginServer.security.service.MemberDetailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,21 +10,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Log4j2
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-
-    @Autowired
-    private MemberDetailService memberDetailsService;
+    private final MemberDetailService memberDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    public ApiCheckFilter apiCheckFilter(){
+        //URI 패턴 추가
+        return new ApiCheckFilter("/notes/**/*");
     }
 
     @Bean
@@ -37,6 +45,17 @@ public class SecurityConfig {
         httpSecurity.authenticationManager(authenticationManager);
 
         httpSecurity.formLogin();
+
+        httpSecurity.csrf().disable();
+
+        // JWT를 사용할 것이며 서버가 나눠져 있으므로 해제
+        httpSecurity
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+        // 인증 처리전 확인
+        httpSecurity.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
