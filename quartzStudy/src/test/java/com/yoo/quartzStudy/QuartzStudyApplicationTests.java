829b0ae5717cfc241d64118f8e33db323b77277f
yoo <edel1212@naver.com>
2023-06-11 16:44:58 +0900

    Quartz 프로젝트 추가

---
+++ quartzStudy/src/test/java/com/yoo/quartzStudy/QuartzStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.quartzStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
