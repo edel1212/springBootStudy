1f1750dc1b4874c98b78729261f093980b18999f
yoo <edel1212@naver.com>
2023-06-12 22:57:56 +0900

    add comment

--- quartzStudy/src/main/java/com/yoo/quartzStudy/config/QuartzConfig.java
+++ quartzStudy/src/main/java/com/yoo/quartzStudy/config/QuartzConfig.java
@@ -2,7 +2,6 @@ package com.yoo.quartzStudy.config;
 
 import com.yoo.quartzStudy.job.TestJobA;
 import com.yoo.quartzStudy.job.TestJobB;
-import lombok.RequiredArgsConstructor;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Trigger;
@@ -10,10 +9,6 @@ import org.quartz.TriggerBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
 import java.util.TimeZone;
 
@@ -21,6 +16,13 @@ import static org.quartz.CronScheduleBuilder.cronSchedule;
 
 @Configuration
 public class QuartzConfig {
+
+    /**
+     * 따라서 간단한 스케줄링 작업이 필요하고 Spring Framework를 사용하고 있다면 @Scheduled를 사용할 수 있습니다.
+     * 그러나 복잡한 스케줄링 작업이나 분산 환경에서 작업을 관리해야 하는 경우 Quartz를 사용하는 것이 더 적합할 수 있습니다.
+     *
+     * -https://advenoh.tistory.com/51
+     * */
 
     @Bean
     public JobDetail jobDetailA() {
@@ -32,9 +34,11 @@ public class QuartzConfig {
     }
 
     @Bean
-    public Trigger tistoryTrigger(@Qualifier("jobDetailA") JobDetail job) {
-        return TriggerBuilder.newTrigger().forJob(job)
-                .withIdentity("tistory_job_trigger")
+    public Trigger tistoryTrigger(JobDetail jobDetailA) {
+        return TriggerBuilder.newTrigger().forJob(jobDetailA)
+                // 트리거의 이름을 설정합니다. 이 이름은 트리거를 식별하는 데 사용됩니다.
+                // 같은 이름의 트리거를 여러 개 생성할 경우, 마지막에 생성된 트리거가 유지됩니다.
+                .withIdentity("test1")
                 .withSchedule(cronSchedule("/5 * * * * ?")
                         .inTimeZone(TimeZone.getTimeZone("Asia/Seoul")))
                 .build();
@@ -50,8 +54,8 @@ public class QuartzConfig {
     }
 
     @Bean
-    public Trigger tistoryTrigger2(@Qualifier("jobDetailB") JobDetail job) {
-        return TriggerBuilder.newTrigger().forJob(job)
+    public Trigger tistoryTrigger2(JobDetail jobDetailB) {
+        return TriggerBuilder.newTrigger().forJob(jobDetailB)
                 .withIdentity("test2")
                 .withSchedule(cronSchedule("/5 * * * * ?")
                         .inTimeZone(TimeZone.getTimeZone("Asia/Seoul")))
