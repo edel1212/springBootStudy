f928afa5d8a01bc516f677e19d82842df5be8779
yoo <edel1212@naver.com>
2023-06-13 21:31:16 +0900

    Uppercamel case test Code

---
+++ webClientServer/serverRes/src/main/java/com/yoo/serverRes/controller/UpperController.java
@@ -0,0 +1,46 @@
+package com.yoo.serverRes.controller;
+
+import com.yoo.serverRes.dto.UpperDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@RestController
+@Log4j2
+@RequestMapping("/upper")
+public class UpperController {
+
+    @GetMapping
+    public ResponseEntity<List<UpperDTO>> getList(){
+
+        List<UpperDTO> result = new ArrayList<>();
+
+        IntStream.rangeClosed(1, 100).forEach(idx->{
+            UpperDTO dto = UpperDTO.builder().SakuraNum("Hahahah"+ idx)
+                    .FlagBool(idx%2 == 0)
+                    .AppleID("BlackGom")
+                    .NumberCd(Long.valueOf(idx))
+                    .build();
+            result.add(dto);
+        });
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<UpperDTO> getItem(@PathVariable Long code){
+        return ResponseEntity.ok(UpperDTO.builder().SakuraNum("Hahahah")
+                .FlagBool(true)
+                .AppleID("BlackGom")
+                .NumberCd(code)
+                .build());
+    }
+
+}
---
+++ webClientServer/serverRes/src/main/java/com/yoo/serverRes/dto/UpperDTO.java
@@ -0,0 +1,26 @@
+package com.yoo.serverRes.dto;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+public class UpperDTO {
+    private String AppleID;
+
+    private String SakuraNum;
+
+    private Boolean FlagBool;
+
+    private Long NumberCd;
+
+}
