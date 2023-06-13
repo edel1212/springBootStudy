bc0a9206bd0465f97e7c4ae9a45977751dc4e0c7
yoo <edel1212@naver.com>
2023-06-13 21:33:16 +0900

    뷸필요 코드 삭제

--- webClientServer/serverRes/src/main/java/com/yoo/serverRes/dto/UpperDTO.java
+++ webClientServer/serverRes/src/main/java/com/yoo/serverRes/dto/UpperDTO.java
@@ -2,7 +2,6 @@ package com.yoo.serverRes.dto;
 
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
