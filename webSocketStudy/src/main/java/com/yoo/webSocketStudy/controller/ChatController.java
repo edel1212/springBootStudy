cb355bc27282bbf3b36c1ba47af9972f6f8bcafe
yoo <edel1212@naver.com>
2023-06-18 19:04:29 +0900

    불필요 코드 삭제

--- webSocketStudy/src/main/java/com/yoo/webSocketStudy/controller/ChatController.java
+++ webSocketStudy/src/main/java/com/yoo/webSocketStudy/controller/ChatController.java
@@ -1,10 +1,8 @@
 package com.yoo.webSocketStudy.controller;
 
-import com.yoo.webSocketStudy.config.ChatHandler;
 import com.yoo.webSocketStudy.service.SendClientMsgToSocketService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
