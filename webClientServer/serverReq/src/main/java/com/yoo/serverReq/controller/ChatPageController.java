6bafae50193d8a83a9a41c54cce23ba02fac3770
yoo <edel1212@naver.com>
2023-06-18 17:25:46 +0900

    WebSocket Test Page 추가

---
+++ webClientServer/serverReq/src/main/java/com/yoo/serverReq/controller/ChatPageController.java
@@ -0,0 +1,19 @@
+package com.yoo.serverReq.controller;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+@Log4j2
+public class ChatPageController {
+
+    @GetMapping("/chatPage")
+    public void chatPage(){
+        log.info("ChatPag!!");
+    }
+
+}
