85323c2928563474a66fd19b6e5c98ed60f86803
yoo <edel1212@naver.com>
2023-06-27 21:56:14 +0900

    project init

---
+++ batchStudy/src/main/java/com/yoo/batchStudy/BatchStudyApplication.java
@@ -0,0 +1,15 @@
+package com.yoo.batchStudy;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing // batch를 사용하기 위해 필 수 선언
+public class BatchStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchStudyApplication.class, args);
+	}
+
+}
