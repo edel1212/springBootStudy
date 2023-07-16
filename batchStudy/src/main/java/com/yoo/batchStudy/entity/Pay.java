a7719aeb0d8e4f0e8e5a2ee3d3573f59da2da424
yoo <edel1212@naver.com>
2023-07-16 19:32:36 +0900

    Entity 수정

--- batchStudy/src/main/java/com/yoo/batchStudy/entity/Pay.java
+++ batchStudy/src/main/java/com/yoo/batchStudy/entity/Pay.java
@@ -1,7 +1,6 @@
 package com.yoo.batchStudy.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,23 +9,22 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Entity
+@ToString
+@Getter
+@Setter
 @NoArgsConstructor
-@Data
+@Entity
 public class Pay {
-    // DataFormatter
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Long amount;
     private String txName;
     private LocalDateTime txDateTime;
 
-
-    public Pay(Long amount, String txName, String txDateTime){
+    public Pay(Long amount, String txName, String txDateTime) {
         this.amount = amount;
         this.txName = txName;
         this.txDateTime = LocalDateTime.parse(txDateTime, FORMATTER);
@@ -38,5 +36,4 @@ public class Pay {
         this.txName = txName;
         this.txDateTime = LocalDateTime.parse(txDateTime, FORMATTER);
     }
-
 }
