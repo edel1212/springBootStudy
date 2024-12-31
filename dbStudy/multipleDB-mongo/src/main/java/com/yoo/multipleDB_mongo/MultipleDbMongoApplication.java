2cc702045a8f22dc26b1bc8a76d88480719e5ec5
yoo <edel1212@naver.com>
2024-12-31 17:58:57 +0900

    project init

---
+++ dbStudy/multipleDB-mongo/src/main/java/com/yoo/multipleDB_mongo/MultipleDbMongoApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.multipleDB_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDbMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDbMongoApplication.class, args);
+	}
+
+}
---
+++ dbStudy/multipleDB-mongo/src/test/java/com/yoo/multipleDB_mongo/MultipleDbMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.multipleDB_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDbMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
