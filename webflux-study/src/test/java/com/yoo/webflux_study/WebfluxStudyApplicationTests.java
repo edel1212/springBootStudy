1485029b683ef8de832b43966ece85fb3151cf3a
yoo <edel1212@naver.com>
2025-01-11 20:12:46 +0900

    project init

---
+++ webflux-study/src/test/java/com/yoo/webflux_study/WebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.webflux_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
