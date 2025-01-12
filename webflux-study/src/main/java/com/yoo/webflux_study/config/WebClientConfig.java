c2e2e8941d800e8efb297de1393504ac52d1f527
yoo <edel1212@naver.com>
2025-01-12 16:44:38 +0900

    WebClient 설정 추가

--- webflux-study/src/main/java/com/yoo/webflux_study/config/WebClientConfig.java
+++ webflux-study/src/main/java/com/yoo/webflux_study/config/WebClientConfig.java
@@ -5,7 +5,6 @@ import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
